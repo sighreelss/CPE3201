ee1792689574ae5fdfdf300e9a3293323bae8b04
Cyril Andre Durango <165168915+sighreelss@users.noreply.github.com>
2025-05-08 20:17:32 +0800

    Problem A - Poblem Given to Owner

---
+++ Exams/Midterms/ProblemA/MCU1.C
@@ -0,0 +1,89 @@
+// TITLE: 	MPLAB IDE PIC16F977A CODE TEMPLATE
+// CREATOR:	CYRIL ANDRE DURANGO
+
+// INCLUDE LINES UNDER HERE
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// PRAGMA LINES UNDER HERE
+#pragma config FOSC = XT
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config BOREN = ON
+#pragma config LVP = OFF
+#pragma config CPD = OFF
+#pragma config WRT = OFF 	
+#pragma config CP = OFF
+
+//DEFINE LINES UNDER HERE (#define <DATA_NAME> data)
+
+//DECLARED GLOBAL VARIABLES HERE (flags)
+int TMR0_flag = 0;	//TMR0 flag bit
+
+//NECESSARY FUNCTIONS
+void delay(int cnt){	// DESC: SETS PIC DELAY
+	while(cnt--);
+	return;
+}
+
+void initPIC(){			// DESC: INITIALIZES PIC
+	//PORT CONFIGURATIONS (edit and/or remove if necessary)
+	TRISB = 0x01;	PORTB = 0x00;	//PORTB config
+	TRISD = 0x0F;	PORTD = 0x00;	//PORTD config
+	
+	//PERIPHERAL CONFIGURATIONS (edit and/or remove if necessary)
+	INTEDG = 1; 	//TIMER0 CONFIG
+
+	SPBRG = 0x19;	SYNC = 0;	BRGH = 1;	SPEN = 1; //USART CONFIGURATIONS
+	TX9 = 0;	TXEN = 1;	//USART ASYNCH TRANSMIT
+	
+	//INTERRUPT CONFIGURATIONS (remove if necessary)
+	INTE = 1; 	INTF = 0;		//TMR0 Interrupt
+	
+	PEIE = 1; 	//PERIPHERAL INTERRUPT
+	GIE = 1;	//GLOBAL INTERRUPT
+	
+}
+
+void interrupt ISR(){	// DESC: INTERRUPT FUNCTION
+	GIE = 0;
+	if(INTF){
+		INTF = 0;
+		TMR0_flag = 1;
+	}
+	GIE = 1;
+}
+
+void main(){			// DESC: MAIN FUNCTION
+	initPIC();
+	//foreground routine
+	int keypad_input;
+	while(1){
+		if(TMR0_flag){
+			TMR0_flag = 0;
+			int input = (PORTD) & 0x0F;
+			switch(input){	
+				case 0x00: keypad_input = 0X01; break;
+				case 0x01: keypad_input = 0X02; break;
+				case 0x02: keypad_input = 0X03; break;
+				case 0x04: keypad_input = 0X04; break;
+				case 0x05: keypad_input = 0X05; break;
+				case 0x06: keypad_input = 0X06; break;
+				case 0x08: keypad_input = 0X07; break;
+				case 0x09: keypad_input = 0X08; break;
+				case 0x0A: keypad_input = 0X09; break;
+				case 0x0C: keypad_input = 0x2A; break;
+				case 0x0D: keypad_input = 0X00; break;
+				case 0x0E: keypad_input = 0x2B; break;
+				default: keypad_input = 0X0F; break;
+			}
+
+			TXREG = keypad_input;
+		}
+
+	}
+}
+
+
---
+++ Exams/Midterms/ProblemA/MCU2.C
@@ -0,0 +1,144 @@
+#include <xc.h>
+
+//PRAGMA LINES UNDER HERE
+#pragma config FOSC = XT
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config BOREN = ON
+#pragma config LVP = OFF
+#pragma config CPD = OFF
+#pragma config WRT = OFF
+#pragma config CP = OFF
+
+void delay(int max){
+	int cnt;
+	for(cnt=0;cnt<max;cnt++); // delay	
+
+	return;
+}
+
+void instCtrl(unsigned char data){
+	PORTB = data;
+	PORTC = 0x02;
+	delay(1000);
+	PORTC = 0x00;
+
+	return;
+}
+
+void dataCtrl(unsigned char data){
+	PORTB = data;
+	PORTC = 0x03;
+	delay(1000);
+	PORTC = 0x01;
+
+	return;
+}
+
+void initLCD(){
+	instCtrl(0x38);
+	instCtrl(0x00);
+	instCtrl(0x01);
+	instCtrl(0x06);
+	instCtrl(0x0C);
+	
+	return;
+}
+
+
+void main(void) 
+{ 
+	SPBRG = 0x19;	// 9.6K baud rate @ FOSC=4MHz, asynchronous high speed 
+					// (see formula in Table 10-1) 
+	SYNC = 0; 		
+	SPEN = 1; 		
+	RX9 = 0; 		
+	BRGH = 1; 		
+	CREN = 1; 
+		
+	TRISB = 0x00; 
+	TRISC = 0xF0;
+	TRISD = 0x00;	
+	PORTB = 0x00; 
+
+	unsigned int data, output;
+	long VAL = 0; 
+	int i;
+	int DATA[6] = {0};
+		
+	int temp = 0;
+
+	int firstnum_flag = 0;
+	int secondnum_flag = 0;
+	int add_flag = 0;
+	int multiply_flag = 0;
+	
+	long firstnum = 0;
+	long secondnum = 0;
+	int temp_1st_arr[3] = {0};
+	int temp_2nd_arr[3] = {0};
+
+	initLCD();
+	instCtrl(0xA2);
+	for(i = 5; i >= 0; i--){
+		dataCtrl('-');
+	}
+	while(1) {
+		instCtrl(0xA2);
+		for(i = 5; i >= 0; i--){
+			dataCtrl('-');
+		}
+		instCtrl(0xE2);
+		for(i = 5; i >= 0; i--){
+			dataCtrl(DATA[i] + '0');
+		}
+		
+		while(!RCIF);
+		temp = RCREG;
+		if (firstnum_flag < 3 && temp < 10){
+			instCtrl(0x91 + firstnum_flag);
+			temp_1st_arr[firstnum_flag] = temp;
+			dataCtrl(temp_1st_arr[firstnum_flag] + '0');
+			firstnum = firstnum*10 + temp_1st_arr[firstnum_flag];
+			firstnum_flag++;
+		}
+		
+		else if (secondnum_flag < 3 && firstnum_flag == 3 && temp < 10){
+			instCtrl(0xD1 + secondnum_flag);
+			temp_2nd_arr[secondnum_flag] = temp;
+			dataCtrl(temp_2nd_arr[secondnum_flag]+'0');
+			secondnum = secondnum*10 + temp_2nd_arr[secondnum_flag];
+			secondnum_flag++;
+		}
+	
+		else if (firstnum_flag == 3 && secondnum_flag ==3){
+			instCtrl(0xCE);
+			if(temp == 0x2A){
+				multiply_flag = 1;	
+				dataCtrl('x');
+				VAL = firstnum * secondnum;
+			}
+			else if(temp == 0x2B){
+				add_flag = 1;
+				dataCtrl('+');
+				VAL = firstnum + secondnum;
+			}
+		} 
+		
+		if (add_flag || multiply_flag){
+			for(i = 0; i < 6; i++){
+				DATA[i] = VAL % 10;
+				VAL = VAL/10;
+			}
+			firstnum = 0;
+			secondnum = 0;	
+			
+			firstnum_flag = 0;
+			secondnum_flag = 0;
+			temp = 0;
+			
+			add_flag = 0;
+			multiply_flag = 0;
+		}
+	}
+} 
